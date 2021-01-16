32e39601b687fbc244c267d6a6198f2ed5d9d45b
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2021-01-16 21:30:47 +0100

    Added numerical input

---
+++ CistercianNumeralMaker.py
@@ -0,0 +1,29 @@
+# Limits determined by the Cistercian numeral system
+LOWER_LIMIT = 0
+UPPER_LIMIT = 9999
+
+def input_is_valid (input_string):
+    if (not(input_string.isnumeric())):
+        return False
+    input_integer = int(input_string)
+    if (not(isinstance(input_integer, int))):
+        return False
+    if (input_integer < LOWER_LIMIT):
+        return False
+    if (input_integer > UPPER_LIMIT):
+        return False
+    return True
+
+def main():
+    print("\nWelcome to Cistercian Numeral Maker")
+
+    input_string = "-1"
+
+    while (not(input_is_valid(input_string))):
+        print("Please enter a integer number on the range [0, 9999]")
+        input_string = input()
+
+    print("Introduced number is {}".format(input_string))
+
+if __name__ == "__main__":
+    main()

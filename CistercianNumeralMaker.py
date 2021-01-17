bd20213075cf41ff44ff9223c4a8559ab3ead9ed
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2021-01-17 01:26:17 +0100

    Finished cistercian numeral algorithm with temporary source images

--- CistercianNumeralMaker.py
+++ CistercianNumeralMaker.py
@@ -1,3 +1,7 @@
+import sys
+from PIL import Image
+from PIL import ImageOps
+
 # Limits determined by the Cistercian numeral system
 LOWER_LIMIT = 0
 UPPER_LIMIT = 9999
@@ -38,10 +42,43 @@ def getNumberDecomposition(numberString):
             decomposition.append(numberString[len(numberString) - (i + 1)])
     return decomposition
 
+def numberString2Integers(list):
+    integerList = []
+    for element in list:
+        integerList.append(int(element))
+    return integerList
+
+def composeNumeralImage(decomposition):
+    background = Image.open('test_bg.png')
+    # Background image needs conversion to RGBA mode
+    background = background.convert('RGBA')
+    base = Image.open('test_0.png')
+    unitsImages = []
+
+    for i in range(1,10):
+        filename = "test_{}.png".format(i)
+        unitsImages.append(Image.open(filename))
+
+    out = Image.alpha_composite(background, base)
+
+    if (decomposition[0] > 0):
+        out = Image.alpha_composite(out, unitsImages[(decomposition[0])-1])
+
+    if (len(decomposition) > 1 and decomposition[1] > 0):
+        out = Image.alpha_composite(out, ImageOps.mirror(unitsImages[(decomposition[1])-1]))
+
+    if (len(decomposition) > 2 and decomposition[2] > 0):
+        out = Image.alpha_composite(out, ImageOps.flip(unitsImages[(decomposition[2])-1]))
+
+    if (len(decomposition) > 3 and decomposition[3] > 0):
+        out = Image.alpha_composite(out, ImageOps.mirror(ImageOps.flip(unitsImages[(decomposition[3])-1])))
+
+    out.show()
 
 def main():
     inputString = getInput()
     decomposition = getNumberDecomposition(inputString)
+    composeNumeralImage(numberString2Integers(decomposition))
 
 if __name__ == "__main__":
     main()
